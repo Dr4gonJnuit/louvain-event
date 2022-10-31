3da54863bdf84cf4f528d25a3948b237a0b89dbb
Jonas Bette <jonas.bette@student.uclouvain.be>
2022-10-31 16:16:28 +0100

    Correction typo dans database et ajout de code permettant de créer deux tables (User and User_Email)

--- database.js
+++ database.js
@@ -1,12 +1,40 @@
 // Import Sequelize
+const { name } = require('ejs');
 const { Sequelize, DataTypes, Model } = require('sequelize')
 
 // Creation of database link
 const sequelize = new Sequelize({
-    dialect: " sqlite ",
-    storage: " preparatoryproject . sqlite "
+    dialect: "sqlite",
+    storage: "preparatoryproject.sqlite"
 })
 
-class Utilisateur extends Model{}
+class User extends Model{}
 
-class Incident extends Model{}
+class User_Email extends Model{}
+
+class Event extends Model{}
+
+User.init({
+    name: {
+        type: DataTypes.TEXT,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+    },
+    pswd: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+}, { sequelize });
+
+User_Email({
+    mail: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true,
+        references: {
+            model: User,
+            key: 'name'
+        }
+    }
+}, { sequelize });
